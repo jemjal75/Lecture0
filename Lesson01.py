22ee711eb687f3a792efa7c72d255e8b1087a9ee
Jalal jemison <jalal@jalaljemison.com>
2019-04-03 14:17:26 -0700

    added the py lesson' tacos '

---
+++ Lesson01.py
@@ -0,0 +1,19 @@
+import random
+print(random.random())
+print(random.randrange(1,1000,1))
+
+
+randomNum=random.randrange(1,1000,1)
+print(randomNum)
+int(randomNum)
+
+randomList=list("taco")
+
+x_list=[]
+i = 0
+while i<100:
+    x_list.append(i)
+    i += 1
+
+print(x_list)
+
